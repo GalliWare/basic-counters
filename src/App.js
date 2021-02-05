ef8d6b9d86f30afb5c14d676d653d221af6bbf03
Craig Gallimore <c.gallimore.it@gmail.com>
2021-02-05 11:48:45 +0200

    Update of app to use Counter as generic component and render array of Counters, add a element so we can add counters to a max of 6. added logic so enter and onclick trigger the adding of the counter. Add button is removed when we reach 6 counters. TODO: improve style of counters with Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { useState } from "react"
 import Counter from "./Counter"
-// TODO: Once Counter modified, create AddCounter component to add a Counter in App with props.  
+// TODO: Style with Bootstrap
+
 function App() {
+  const [addButton, setAddButton] = useState(true)
+  const [textInput, setTextInput] = useState("")
+  const [counters, setCounters] = useState([<Counter name="Runs" key="Runs" />])
+
+  function addCounter(event) {
+    if ((event._reactName === "onKeyDown" && event.code === "Enter") || event._reactName === "onClick") {
+      console.log(event)
+      const countName = event.target.value
+      let countersTemp = counters
+      countersTemp.push(<Counter name={countName} key={countName} />)
+      setCounters(countersTemp)
+      setTextInput("")
+      if (counters.length === 6) { setAddButton(false) }
+    }
+  }
+
   return (
-    <Counter/>
+    addButton ?
+      <div>
+        <div>
+          {counters}
+        </div>
+        <div>
+          <div className="w-25 mx-auto p-3 mb-5 bg-white rounded">
+            <input typeof="text" value={textInput} placeholder="Counter Name Here..." onKeyDown={addCounter} onChange={(event) => { setTextInput(event.target.value) }}></input>
+            <button className="btn btn-outline-primary col-sm" value={textInput} onClick={addCounter}> Add Counter +</button>
+          </div>
+        </div>
+      </div > :
+      <div>
+        {counters}
+      </div>
   )
 }
 
