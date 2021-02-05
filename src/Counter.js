8906b32f6cf47845324df115e1ba2b5af07dbab9
Craig Gallimore <c.gallimore.it@gmail.com>
2021-02-05 11:46:22 +0200

    updated Counter to take a name prop, so can have a different heading. Removed the 100% height and correct the class to classname

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react"
+// TODO: Style with Bootstrap
 
-function Counter() {
-  // TODO: modify to take props from App so we can use as generic component
+function Counter(props) {
   const [count, setCount] = useState(0)
   return (
-    <div class="h-100">
-      <div class="w-25 mx-auto shadow-lg p-3 mb-5 bg-white rounded">
-        <h1 class="text-center">Counter</h1>
-        <h2 class="text-center">{count}</h2>
-        <div class="btn-group btn-group-lg col-sm">
-          <button onClick={() => setCount(count - 1)} class="btn btn-outline-primary">-</button>
-          <button onClick={() => setCount(count + 1)} class="btn btn-outline-primary">+</button>
+    <div>
+      <div className="w-25 mx-auto shadow-lg p-3 mb-5 bg-white rounded">
+        <h1 className="text-center">{props.name}</h1>
+        <h2 className="text-center">{count}</h2>
+        <div className="btn-group btn-group-lg col-sm">
+          <button onClick={() => setCount(count - 1)} className="btn btn-outline-primary">-</button>
+          <button onClick={() => setCount(count + 1)} className="btn btn-outline-primary">+</button>
         </div>
       </div>
     </div>
